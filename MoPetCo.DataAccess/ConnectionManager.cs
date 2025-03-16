2ea315cf8dddbfbbc1d28de13668769babdd9939
douglas-i <hdouglasu@gmail.com>
2025-03-16 11:45:14 -0600

    add connection manager

---
+++ MoPetCo.DataAccess/ConnectionManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using MoPetCo.DataAccess.Interfaces;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace MoPetCo.DataAccess
+{
+    public class ConnectionManager : IConnectionManager
+    {
+        public const string connectionStringKey = "MoPetCoConectionString_BD";
+        private readonly IConfiguration configuration;
+
+        public ConnectionManager(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IDbConnection GetConnectionString(string key)
+        {            
+            return new SqlConnection(ConfigurationExtensions.GetConnectionString(configuration, $"{ key }"));
+        }
+    }
+}
--- MoPetCo.Service/Program.cs
+++ MoPetCo.Service/Program.cs
@@ -1,3 +1,6 @@
+using MoPetCo.DataAccess;
+using MoPetCo.DataAccess.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configuring services for the application
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddSingleton<IConnectionManager, ConnectionManager>();
 
 var app = builder.Build();
 
