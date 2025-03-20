e1b79a1419f43c22fc89dfae148f843ce286ecd2
douglas-i <hdouglasu@gmail.com>
2025-03-19 21:30:28 -0600

    Remove unnecessary using directives

--- MoPetCo.BusinessLogic.Interfaces/IContacto.cs
+++ MoPetCo.BusinessLogic.Interfaces/IContacto.cs
@@ -1,14 +1,9 @@
 ﻿using MoPetCo.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoPetCo.BusinessLogic.Interfaces
 {
     public interface IContacto
     {
-        Task<Response<Models.Contacto>> EnviarEmailAsync(Models.Contacto contacto);
+        Task<Response<Contacto>> EnviarEmailAsync(Contacto contacto);
     }
 }
--- MoPetCo.DataAccess.Interfaces/IServicio.cs
+++ MoPetCo.DataAccess.Interfaces/IServicio.cs
@@ -1,17 +1,12 @@
 ﻿using MoPetCo.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoPetCo.DataAccess.Interfaces
 {
     public interface IServicio
     {
-        Task<Response<Models.Servicio>> GuardarServioAsync(Models.Servicio servicio);
-        Task<Response<Models.RangoPeso>> GuardarRangoPesoAsync(Models.RangoPeso rangoPeso);
-        Task<Response<Models.Precio>> GuardarPrecioAsync(Models.Precio precio);
-        Task<Response<IEnumerable<Models.Servicio>>> ObtenerServiciosAsync();
+        Task<Response<Servicio>> GuardarServioAsync(Servicio servicio);
+        Task<Response<RangoPeso>> GuardarRangoPesoAsync(RangoPeso rangoPeso);
+        Task<Response<Precio>> GuardarPrecioAsync(Precio precio);
+        Task<Response<IEnumerable<Servicio>>> ObtenerServiciosAsync();
     }
 }
